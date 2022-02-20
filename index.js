73a716cfe26b4ea5f68613dee7289dc606c62320
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-02-19 16:37:36 -0800

    made class files

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('sheeeit')
+const inquirer = require('inquirer')
+const fs = require("fs");
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+function maketeam() {
+    inquirer.prompt([{
+        message: "What's the team called?"
+        name: "teamname"
+    },
+    {
+        type: "list"
+        message: "select role"
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member id",
+        name: "id"
+    },
+    {
+        message: "Enter team member email address",
+        name: "email"
+    }])
+
+   
+
+}
