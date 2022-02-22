f758657732ee7056ab2f25c7b3f034fb178b161b
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-02-21 20:24:08 -0800

    got the inquirer function down. next thing is making the generateHTML functions. then the bloody tests

--- index.js
+++ index.js
@@ -6,20 +6,12 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 
+
 function maketeam() {
-    inquirer.prompt([{
-        message: "What's the team called?"
-        name: "teamname"
-    },
+    inquirer.prompt([
     {
-        type: "list"
-        message: "select role"
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager"
-        ],
-        name: "role"
+        message: "What's the member's name?",
+        name: "name"
     },
     {
         message: "Enter team member id",
@@ -28,8 +20,65 @@ function maketeam() {
     {
         message: "Enter team member email address",
         name: "email"
-    }])
+    },
+    {
+        type: "list",
+        message: "select role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    }
+    ])
+    .then(function({role}){
+        switch(role) {
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Manager":
+                addManager();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            default:
+                generateHTML();
+        }
+        }
+    )
+ }
 
-   
-
+function addEngineer(){
+    inquirer.prompt ([
+       {
+           type: "input",
+           name: "github",
+           message: "What is the engineer's GitHub username?"
+         }
+    
+       ])
 }
+function addManager(){
+    inquirer.prompt ([
+       {
+           type: "input",
+           name: "managerOfficeNumber",
+           message: "What is the manager's office number?"
+         }
+    
+       ])
+}
+ function addIntern(){
+     inquirer.prompt ([
+        {
+            type: "input",
+            name: "school",
+            message: "Intern's school?"
+          }
+     
+        ])
+}
+
+maketeam();
